f9472413bf5184eb5114630996366bd3d3cea09e
Jared Smith <j.danielsmith451@gmail.com>
2018-11-05 15:04:57 -0800

    Submission for checkpoint list chatrooms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js'
+
+
+var config = {
+    apiKey: "AIzaSyAEukQq15t4I7D8yeLQk3TGr5h2CB_HD2Y",
+    authDomain: "bloc-chat-react-69908.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-69908.firebaseio.com",
+    projectId: "bloc-chat-react-69908",
+    storageBucket: "bloc-chat-react-69908.appspot.com",
+    messagingSenderId: "241483774953"
+  };
+
+firebase.initializeApp(config)
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <main>
+          <RoomList firebase={firebase} />
+        </main>
       </div>
     );
   }
